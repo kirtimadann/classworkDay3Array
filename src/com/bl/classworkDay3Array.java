bea0dd85910dde3416e1637ee24e3ac63c35a8f4
lunainfotec <kirtimadan1997@gmail.com>
2023-04-05 17:34:03 +0530

    added : master

---
+++ src/com/bl/classworkDay3Array.java
@@ -0,0 +1,19 @@
+package com.bl;
+
+public class classworkDay3Array {
+    public static void main(String[] args) {
+        int [] arr = new int[]{1,2,3,4,5,6,7,7};
+
+        System.out.println("Java Program to print the elements of an array :");
+
+        for (int i=0; i<arr.length; i++){
+
+            System.out.println(arr[i]);
+
+        }
+    }
+}
+
+
+
+
